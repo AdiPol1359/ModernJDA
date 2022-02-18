317dfa6486f7821f40a434fc6dfcabca6be514db
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-18 22:13:56 +0100

    Create new command event

--- src/main/java/me/adipol/modernjda/command/events/CommandExceptionEvent.java
+++ src/main/java/me/adipol/modernjda/command/events/CommandExceptionEvent.java
@@ -1,2 +1,15 @@
-package me.adipol.modernjda.command.events;public class CommandExceptionEvent {
+package me.adipol.modernjda.command.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import me.adipol.modernjda.command.AbstractCommand;
+import me.adipol.modernjda.event.CustomEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+@AllArgsConstructor
+@Getter
+public class CommandExceptionEvent extends CustomEvent {
+    private final Exception exception;
+    private final AbstractCommand command;
+    private final MessageReceivedEvent event;
 }
