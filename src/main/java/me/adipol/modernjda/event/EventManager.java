3a07f0d494563de97712ada587e1da4b585c27a8
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-18 15:56:35 +0100

    Small changes in custom events

--- src/main/java/me/adipol/modernjda/event/EventManager.java
+++ src/main/java/me/adipol/modernjda/event/EventManager.java
@@ -1,5 +1,7 @@
 package me.adipol.modernjda.event;
 
+import net.dv8tion.jda.api.events.GenericEvent;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,16 +17,24 @@ public class EventManager {
         listeners.addAll(List.of(listener));
     }
 
-    public <T> void callEvent(T t) {
+    public <T extends GenericEvent> void callEvent(T event) {
+        executeEvent(event);
+    }
+
+    public <T extends CustomEvent> void callEvent(T event) {
+        executeEvent(event);
+    }
+
+    private <T> void executeEvent(T event) {
         listeners.forEach(listener -> {
             List.of(listener.getClass().getMethods()).forEach(method -> {
                 if(method.getAnnotation(EventHandler.class) == null) {
                     return;
                 }
 
-                if(method.getParameters().length > 0 && method.getParameters()[0].getType().equals(t.getClass())) {
+                if(method.getParameters().length > 0 && method.getParameters()[0].getType().equals(event.getClass())) {
                     try {
-                        method.invoke(listener, t);
+                        method.invoke(listener, event);
                     } catch (Exception ex) {
                         ex.printStackTrace();
                     }
