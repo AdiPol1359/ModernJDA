e319c70b4c3dbac19bcc1c3525c90c1bfbdc6cff
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-20 14:12:36 +0100

    Add check update

--- src/main/java/me/adipol/modernjda/util/VersionInfo.java
+++ src/main/java/me/adipol/modernjda/util/VersionInfo.java
@@ -1,5 +1,19 @@
 package me.adipol.modernjda.util;
 
+import com.google.gson.JsonArray;
+import me.adipol.modernjda.request.Request;
+import me.adipol.modernjda.request.RequestMethod;
+
 public interface VersionInfo {
     String VERSION = "1.0.0";
+
+    static boolean checkUpdate() {
+        JsonArray jsonArray = Request.builder()
+                .url("https://api.github.com/repos/dzikoysk/reposilite/releases")
+                .method(RequestMethod.GET)
+                .build()
+                .send(JsonArray.class);
+
+        return !jsonArray.get(0).getAsJsonObject().get("tag_name").getAsString().equals(VERSION);
+    }
 }
