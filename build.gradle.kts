49febd047b6442d6e1cb47f83d97f44e87aa3c1b
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-20 11:38:27 +0100

    Change gradle to kotlin language

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    java
+    `maven-publish`
+}
+
+group = "me.adipol.modernjda"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-alpha.5")
+
+    annotationProcessor("org.projectlombok:lombok:1.18.22")
+    compileOnly("org.projectlombok:lombok:1.18.22")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "local-repository"
+            url = uri("https://repo.projectcode.pl/releases")
+
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_TOKEN")
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+}
