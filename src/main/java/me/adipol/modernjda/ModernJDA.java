57a8f26176c79e049516082cd44923826ed686e5
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-20 14:12:23 +0100

    Add check update

--- src/main/java/me/adipol/modernjda/ModernJDA.java
+++ src/main/java/me/adipol/modernjda/ModernJDA.java
@@ -3,6 +3,7 @@ package me.adipol.modernjda;
 import lombok.Getter;
 import me.adipol.modernjda.command.CommandManager;
 import me.adipol.modernjda.event.EventManager;
+import me.adipol.modernjda.util.VersionInfo;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -27,6 +28,13 @@ public class ModernJDA {
 
         commandManager = new CommandManager();
         eventManager = new EventManager();
+
+        if(VersionInfo.checkUpdate()) {
+            System.out.println("]============== ( ModernJDA ) ==============[");
+            System.out.println("* A new version of ModernJDA is available!  *");
+            System.out.println("* https://github.com/AdiPol1359/ModernJDA   *");
+            System.out.println("]============== ( ModernJDA ) ==============[");
+        }
     }
 
     public void setActivity(Activity activity) {
