351eaee9cbf3b73d9cd86e587cd4cdfff62c797b
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-20 14:16:12 +0100

    Change the way to get command map

--- src/main/java/me/adipol/modernjda/EventListener.java
+++ src/main/java/me/adipol/modernjda/EventListener.java
@@ -10,7 +10,6 @@ import me.adipol.modernjda.command.events.CommandNotFoundEvent;
 import me.adipol.modernjda.event.EventManager;
 import me.adipol.modernjda.util.MemberUtil;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -29,43 +28,39 @@ public class EventListener extends ListenerAdapter {
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if(event.getAuthor().isBot() || event.getMember() == null) return;
 
+        Member member = event.getMember();
         String message = event.getMessage().getContentRaw();
         String[] args = message.split(" ");
 
-        Member member = event.getMember();
-        MessageChannel channel = event.getChannel();
-
         eventManager.callEvent(event);
 
-        commandManager.getCommandMaps().forEach(commandMap -> {
-            if(message.startsWith(commandMap.getPrefix())) {
-                String name = args[0].substring(commandMap.getPrefix().length());
-                Optional<AbstractCommand> optionalCommand = commandMap.getCommand(name);
+        commandManager.getCommandMaps().stream().filter(map -> message.startsWith(map.getPrefix())).findFirst().ifPresent(commandMap -> {
+            String name = args[0].substring(commandMap.getPrefix().length());
+            Optional<AbstractCommand> optionalCommand = commandMap.getCommand(name);
 
-                if(optionalCommand.isEmpty()) {
-                    eventManager.callEvent(new CommandNotFoundEvent(commandMap, event));
+            if(optionalCommand.isEmpty()) {
+                eventManager.callEvent(new CommandNotFoundEvent(commandMap, event));
+                return;
+            }
+
+            AbstractCommand command = optionalCommand.get();
+
+            if(commandMap.getPermissions().length > 0 || command.getAnnotation().permissions().length > 0) {
+                if(!MemberUtil.hasRole(member, commandMap.getPermissions()) && !MemberUtil.hasRole(member, command.getAnnotation().permissions())) {
+                    eventManager.callEvent(new CommandMissingPermissionEvent(commandMap, command, event));
                     return;
                 }
+            }
 
-                AbstractCommand command = optionalCommand.get();
+            CommandExecuteEvent commandExecuteEvent = new CommandExecuteEvent(commandMap, command, event);
 
-                if(commandMap.getPermissions().length > 0 || command.getAnnotation().permissions().length > 0) {
-                    if(!MemberUtil.hasRole(member, commandMap.getPermissions()) && !MemberUtil.hasRole(member, command.getAnnotation().permissions())) {
-                        eventManager.callEvent(new CommandMissingPermissionEvent(commandMap, command, event));
-                        return;
-                    }
-                }
+            eventManager.callEvent(commandExecuteEvent);
 
-                CommandExecuteEvent commandExecuteEvent = new CommandExecuteEvent(commandMap, command, event);
-
-                eventManager.callEvent(commandExecuteEvent);
-
-                if(!commandExecuteEvent.isCancelled()) {
-                    try {
-                        command.handleCommand(member, channel, event, Arrays.copyOfRange(args, 1, args.length));
-                    } catch(Exception ex) {
-                        eventManager.callEvent(new CommandExceptionEvent(ex, command, event));
-                    }
+            if(!commandExecuteEvent.isCancelled()) {
+                try {
+                    command.handleCommand(member, event.getChannel(), event, Arrays.copyOfRange(args, 1, args.length));
+                } catch(Exception ex) {
+                    eventManager.callEvent(new CommandExceptionEvent(ex, command, event));
                 }
             }
         });
