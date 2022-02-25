18b0b5c354b19dbedd089b33e0eae798756f0620
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-25 16:38:15 +0100

    Create client builder

--- src/main/java/me/adipol/modernjda/ClientBuilder.java
+++ src/main/java/me/adipol/modernjda/ClientBuilder.java
@@ -1,2 +1,45 @@
-package me.adipol.modernjda;public class ClientBuilder {
+package me.adipol.modernjda;
+
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientBuilder {
+
+    private final List<GatewayIntent> intents = new ArrayList<>();
+    private final List<CacheFlag> flags = new ArrayList<>();
+
+    private boolean updates = true;
+
+    private String token;
+
+    public ClientBuilder setToken(String token) {
+        this.token = token;
+        return this;
+    }
+
+    public ClientBuilder checkUpdate(boolean updates) {
+        this.updates = updates;
+        return this;
+    }
+
+    public ClientBuilder enableIntent(GatewayIntent intent) {
+        intents.add(intent);
+        return this;
+    }
+
+    public ClientBuilder enableCache(CacheFlag flag) {
+        flags.add(flag);
+        return this;
+    }
+
+    public ModernJDA build() {
+        if(token == null) {
+            throw new RuntimeException("You need to set the token before build client!");
+        }
+
+        return new ModernJDA(token, updates, intents, flags);
+    }
 }
