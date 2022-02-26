b5adfc0f722c35e6cfb19a2fbc2fb4ad5aa0d353
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-26 15:49:48 +0100

    Create command cool down event

--- src/main/java/me/adipol/modernjda/command/events/CommandCoolDownEvent.java
+++ src/main/java/me/adipol/modernjda/command/events/CommandCoolDownEvent.java
@@ -1,2 +1,15 @@
-package me.adipol.modernjda.command.events;public class CommandCoolDownEvent {
+package me.adipol.modernjda.command.events;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import me.adipol.modernjda.command.AbstractCommand;
+import me.adipol.modernjda.event.CustomEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+@AllArgsConstructor
+@Getter
+public class CommandCoolDownEvent extends CustomEvent {
+    private final long coolDown;
+    private final AbstractCommand command;
+    private final MessageReceivedEvent messageReceivedEvent;
 }
