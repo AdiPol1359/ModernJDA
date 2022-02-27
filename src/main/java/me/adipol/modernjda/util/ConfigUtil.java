61d63ef9180ed0d8cfe914bc64ba2208a3f2ab60
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-27 12:00:27 +0100

    Create ConfigUtil class

--- src/main/java/me/adipol/modernjda/util/ConfigUtil.java
+++ src/main/java/me/adipol/modernjda/util/ConfigUtil.java
@@ -1,2 +1,50 @@
-package me.adipol.modernjda.util;public class ConfigUtil {
+package me.adipol.modernjda.util;
+
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+import me.adipol.modernjda.config.Config;
+import org.yaml.snakeyaml.DumperOptions;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.Constructor;
+import org.yaml.snakeyaml.nodes.Tag;
+import org.yaml.snakeyaml.representer.Representer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.PrintWriter;
+
+@UtilityClass
+public class ConfigUtil {
+
+    private static final Representer representer;
+    private static final DumperOptions dumperOptions;
+
+    static {
+        representer = new Representer();
+        dumperOptions = new DumperOptions();
+
+        dumperOptions.setIndent(2);
+        dumperOptions.setPrettyFlow(false);
+        dumperOptions.setDefaultFlowStyle(DumperOptions.FlowStyle.BLOCK);
+    }
+
+    public static <T extends Config> T loadConfig(String name, Class<T> clazz) {
+        return loadConfig(name, clazz, false);
+    }
+
+    @SneakyThrows
+    public static <T extends Config> T loadConfig(String name, Class<T> clazz, boolean override) {
+        representer.addClassTag(clazz, Tag.MAP);
+
+        name = name + ".yml";
+
+        File file = new File(name);
+        Yaml yaml = new Yaml(new Constructor(clazz), representer, dumperOptions);
+
+        if(!file.exists() || override) {
+            yaml.dump(clazz.getConstructor().newInstance(), new PrintWriter(name));
+        }
+
+        return yaml.load(new FileInputStream(file));
+    }
 }
