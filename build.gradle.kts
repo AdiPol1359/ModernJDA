f7488f18ad887da58ffcda36ae446e3dabd5cab9
AdiPol1359 <adrian.polak20@gmail.com>
2022-02-27 11:59:19 +0100

    Fix dependency scope issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,21 @@
 plugins {
     java
     `maven-publish`
+    `java-library`
 }
 
 group = "me.adipol.modernjda"
-version = "1.0.0"
+version = "1.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-alpha.5")
     implementation("com.google.code.gson:gson:2.9.0")
     implementation("org.yaml:snakeyaml:1.30")
+
+    api("net.dv8tion:JDA:5.0.0-alpha.5")
 
     annotationProcessor("org.projectlombok:lombok:1.18.22")
     compileOnly("org.projectlombok:lombok:1.18.22")
